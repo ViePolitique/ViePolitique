ee58d9da549cdf720ccd35c8df9fbce468e1783a
Filimindji <dh-alias@outlook.com>
2014-08-27 20:48:07 +0000

    Ajout initial

---
+++ ViePolitique.WebApplication/ViePolitique.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ViePolitique.WebApplication.Startup))]
+namespace ViePolitique.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
